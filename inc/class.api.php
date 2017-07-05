18672eb1eb486f7bc977257ab4fb46d3647ecb81
Mauko Maunde <maukoese@gmail.com>
2017-07-05 09:26:47 +0300

    That's So Us

---
+++ inc/class.api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* REST API Class https://github.com/gurhankokcu/phprestserver
+*/
+class _hApi
+{
+    
+    function __construct( $request, $class, $type)
+    {
+        # code...
+    }
+
+    function get($value='')
+    {
+        # code...
+    }
+}
